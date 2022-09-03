f166de214a72c3730d0366f71e15d6937db77f45
LiviuSopon <liviusopon@gmail.com>
2022-09-04 01:33:39 +0300

    feat(api)!: add UNIQUE constraint to phone column

--- backend/src/models.py
+++ backend/src/models.py
@@ -6,7 +6,7 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
-    phone = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(100), nullable=False, unique=True)
 
     def to_json(self):
         return {
