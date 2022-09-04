4e23370aa9555d35b00b5fd04b5db2acf18a6037
LiviuSopon <liviusopon@gmail.com>
2022-09-04 22:36:21 +0300

    test: add basic testing for the four api endpoints

--- backend/tests/test_cases.py
+++ backend/tests/test_cases.py
@@ -13,8 +13,7 @@ class TestReadingData(BaseCase):
             self.db.session.add(test_contact)
             self.db.session.commit()
         req = self.client.get("/all", json={})
-        expected = b'[{"email":"test_email_1","name":"test_name_1","phone":"test_phone_1"}]\n'
-        print(req.data)
+        expected = b'[{"email":"test_email_1","name":"test_name_1","phone":"test_phone_1"}]\n'  # noqa: E501
         assert expected == req.data
         assert req.status_code == 200
 
@@ -29,5 +28,34 @@ class TestAddingData(BaseCase):
                 "phone": "test_phone_1",
             },
         )
-        print(req.data)
-        print("status: ", req.status_code)
+        expected = b'{"email":"test_email_1","name":"test_name_1","phone":"test_phone_1"}\n'  # noqa: E501
+        assert expected == req.data
+        assert req.status_code == 201
+
+
+class TestDeletingData(BaseCase):
+    def test_delete(self):
+        test_contact = Contact(
+            name="test_name_1", email="test_email_1", phone="test_phone_1"
+        )
+        with self.app.app_context():
+            self.db.session.add(test_contact)
+            self.db.session.commit()
+        req = self.client.delete("/test_phone_1", json={})
+        expected = b'{"result":true}\n'
+        assert expected == req.data
+        assert req.status_code == 200
+
+
+class TestUpdatingData(BaseCase):
+    def test_put(self):
+        test_contact = Contact(
+            name="test_name_1", email="test_email_1", phone="test_phone_1"
+        )
+        with self.app.app_context():
+            self.db.session.add(test_contact)
+            self.db.session.commit()
+        req = self.client.put("/test_phone_1", json={"name": "test_name_2"})
+        expected = b'{"email":"test_email_1","name":"test_name_2","phone":"test_phone_1"}\n'  # noqa: E501
+        assert expected == req.data
+        assert req.status_code == 200
